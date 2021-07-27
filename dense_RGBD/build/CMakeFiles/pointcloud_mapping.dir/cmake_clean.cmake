be974b326aef201b32eca34bb984aa0ba11fdbf1
Karen-Cyber <1031307782@qq.com>
2021-07-27 20:08:10 +0800

    2021-07-21

---
+++ dense_RGBD/build/CMakeFiles/pointcloud_mapping.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "../bin/pointcloud_mapping"
+  "../bin/pointcloud_mapping.pdb"
+  "CMakeFiles/pointcloud_mapping.dir/pointcloud_mapping.cpp.o"
+  "CMakeFiles/pointcloud_mapping.dir/pointcloud_mapping.cpp.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/pointcloud_mapping.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
