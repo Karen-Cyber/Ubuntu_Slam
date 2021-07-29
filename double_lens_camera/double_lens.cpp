a7ee65fd42e91daf3af7a6aa7604911236330a1c
Karen-Cyber <1031307782@qq.com>
2021-07-29 16:35:04 +0800

    capture the images from stereo camera

--- double_lens_camera/double_lens.cpp
+++ double_lens_camera/double_lens.cpp
@@ -3,6 +3,7 @@
 using namespace cv;
 
 #include <iostream>
+#include <cstring>
 using namespace std;
 
 int main(int argc, char** argv)
@@ -12,17 +13,24 @@ int main(int argc, char** argv)
     cap.set(CV_CAP_PROP_FRAME_WIDTH,1280);
     cap.set(CV_CAP_PROP_FRAME_HEIGHT,480);
     Mat img_double;
+    int n = 0;
     while (true)
     {
         Mat frame;
 		cap >> frame;
-		// resize(frame, frame, Size(1280, 480));//set size of image
 		Mat leftImage, rightImage;
 		leftImage = frame(Rect(0, 0, frame.size().width / 2, frame.size().height));//split left image
 		rightImage = frame(Rect(frame.size().width / 2, 0, frame.size().width / 2, frame.size().height));//split right image
 		imshow("leftImage", leftImage);//left image
 		imshow("rightImage", rightImage);//right image
-		if (waitKey(15) >= 0) break;
+		if (waitKey(5) == 97)
+        {
+            imwrite("left"  + to_string(n) + ".png", leftImage);
+            imwrite("right" + to_string(n) + ".png", rightImage);
+            n++;
+        }
+        else if (waitKey(5) == 27)
+            break;
     }
 
     return 0;
