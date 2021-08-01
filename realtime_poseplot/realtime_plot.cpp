4fa75de46151b4f469d6e3f734951e9e6d7533a4
Karen-Cyber <1031307782@qq.com>
2021-08-01 13:53:04 +0800

    new real trajectory plot

--- realtime_poseplot/realtime_plot.cpp
+++ realtime_poseplot/realtime_plot.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <vector>
+#include <unistd.h>
 using namespace std;
 
 #include <opencv2/opencv.hpp>
@@ -57,9 +58,20 @@ void pose_estimation_2d2d(
     cv::Mat& cvt
 );
 
-cv::Point2d pixel2cam(const cv::Point2d& p, const cv::Mat& cvK);
+/**
+ * Uncomment this function prototype and the corresponding
+ * function definitions if you want to adopt the 3d-3d pai
+ * rs-matching method
+ */
+// cv::Point2d pixel2cam(const cv::Point2d& p, const cv::Mat& cvK);
 
-// -----------------------------------------------------------------
+void pangolinInit();
+void drawTrajectories(const vector<Eigen::Isometry3d, Eigen::aligned_allocator<Eigen::Isometry3d>>& poses);
+
+void cvR_eiR(const cv::Mat& cvR, Eigen::Matrix3d& eiR);
+void cvt_eit(const cv::Mat& cvt, Eigen::Vector3d& eit);
+
+//! -----------------------------------------------------------------
 
 int main(int argc, char** argv)
 {
@@ -116,6 +128,9 @@ int main(int argc, char** argv)
     Eigen::Vector3d eit;
     cv::Mat cvR;
     cv::Mat cvt;
+    // Poses
+    vector<Eigen::Isometry3d, Eigen::aligned_allocator<Eigen::Isometry3d>> poses;
+    poses.push_back(T);
 
     // Start real time plot
     // Reference preparations
@@ -134,11 +149,8 @@ int main(int argc, char** argv)
     {
         cap >> img_f;
         comput_disparity(img_f, img_l, img_r, img_d, sgbm);
-        // cv::imshow("disparity", img_d / float(96.0 * 16.0));
-        cv::imshow("ref_l", ref_l);
-        cv::imshow("img_l", img_l);
 
-        // Start matching 3D-pairs
+        // Start matching 2D-pairs[BRIEF descriptions]
         find_feature_matches(img_l, ref_l, keypoints_1, keypoints_2, matches);
 
         // for (cv::DMatch& m : matches)
@@ -156,31 +168,36 @@ int main(int argc, char** argv)
         // cout << "3d-3d pairs: " << points3d_1.size() << endl;
         // // pose_estimation_3d3d
         // pose_estimation_3d3d(points3d_2, points3d_1, eiR, eit);
+        // points3d_1.clear();
+        // points3d_2.clear();
 
         // pose_estimation_2d2d
         pose_estimation_2d2d(keypoints_1, keypoints_2, matches, cvR, cvt);
-        cout << "------------\n";
-        cout << "R:\n";
-        cout << cvR << endl;
-        cout << "t:\n";
-        cout << cvt << endl;
+        cvR_eiR(cvR, eiR);
+        cvt_eit(cvt, eit);
+        cout << "eit:\n";
+        cout << eit.matrix() << endl;
+        T.rotate(eiR);
+        if (eit(0, 0) < 0.45 && eit(0, 1) < 0.45 && eit(0, 2) < 0.45)
+            eit = Eigen::Vector3d::Zero();
+        T.pretranslate(eit);
+        cout << "Transformation:\n" << T.matrix() << endl;
+        poses.push_back(T);
 
         // Refresh
         matches.clear();
         keypoints_1.clear();
         keypoints_2.clear();
-        // points3d_1.clear();
-        // points3d_2.clear();
-        // Move forward
-        
-        // ref_d = img_d;
 
         if (cv::waitKey(10) == 97) break;
         ref_l = img_l.clone();
     }
-
+    // Pangolin display poses
+    drawTrajectories(poses);
     return 0;
 }
+
+//! -----------------------------------------------------------------
 
 void comput_disparity(
     const cv::Mat& frame,
@@ -224,7 +241,6 @@ void find_feature_matches(
     }
 }
 
-// Bad output ???
 void pose_estimation_3d3d(
     const vector<cv::Point3f> &points_1,
     const vector<cv::Point3f> &points_2,
@@ -300,10 +316,78 @@ void pose_estimation_2d2d(
     cv::recoverPose(cvE, points_1, points_2, cvR, cvt, 507.9, cv::Point2d(331.3, 244.9));
 }
 
-cv::Point2d pixel2cam(const cv::Point2d& p, const cv::Mat& cvK)
+// cv::Point2d pixel2cam(const cv::Point2d& p, const cv::Mat& cvK)
+// {
+//     return cv::Point2d(
+//         (p.x - cvK.at<double>(0, 2)) / cvK.at<double>(0, 0),
+//         (p.y - cvK.at<double>(1, 2)) / cvK.at<double>(1, 1)
+//     );
+// }
+
+void drawTrajectories(const vector<Eigen::Isometry3d, Eigen::aligned_allocator<Eigen::Isometry3d>>& poses)
 {
-    return cv::Point2d(
-        (p.x - cvK.at<double>(0, 2)) / cvK.at<double>(0, 0),
-        (p.y - cvK.at<double>(1, 2)) / cvK.at<double>(1, 1)
+    // create pangolin window and plot the trajectory
+    pangolin::CreateWindowAndBind("Trajectory Viewer", 1024, 768);
+    glEnable(GL_DEPTH_TEST);
+    glEnable(GL_BLEND);
+    glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+
+    pangolin::OpenGlRenderState s_cam(
+        pangolin::ProjectionMatrix(1024, 768, 500, 500, 512, 389, 0.1, 1000),
+        pangolin::ModelViewLookAt(0, -0.1, -1.8, 0, 0, 0, 0.0, -1.0, 0.0)
     );
+
+    pangolin::View &d_cam = pangolin::CreateDisplay()
+    .SetBounds(0.0, 1.0, 0.0, 1.0, -1024.0f / 768.0f)
+    .SetHandler(new pangolin::Handler3D(s_cam));
+
+    while (pangolin::ShouldQuit() == false)
+    {
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+        d_cam.Activate(s_cam);
+        glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
+        glLineWidth(2);
+        for (size_t i = 0; i < poses.size(); i++) 
+        {
+            // 画每个位姿的三个坐标轴
+            Eigen::Vector3d Ow = poses[i].translation();
+            Eigen::Vector3d Xw = poses[i] * (0.1 * Eigen::Vector3d(1, 0, 0));
+            Eigen::Vector3d Yw = poses[i] * (0.1 * Eigen::Vector3d(0, 1, 0));
+            Eigen::Vector3d Zw = poses[i] * (0.1 * Eigen::Vector3d(0, 0, 1));
+            glBegin(GL_LINES);
+            glColor3f(1.0, 0.0, 0.0);
+            glVertex3d(Ow[0], Ow[1], Ow[2]);
+            glVertex3d(Xw[0], Xw[1], Xw[2]);
+            glColor3f(0.0, 1.0, 0.0);
+            glVertex3d(Ow[0], Ow[1], Ow[2]);
+            glVertex3d(Yw[0], Yw[1], Yw[2]);
+            glColor3f(0.0, 0.0, 1.0);
+            glVertex3d(Ow[0], Ow[1], Ow[2]);
+            glVertex3d(Zw[0], Zw[1], Zw[2]);
+            glEnd();
+        }
+        // 画出连线
+        for (size_t i = 0; i < poses.size(); i++)
+        {
+            glColor3f(0.0, 0.0, 0.0);
+            glBegin(GL_LINES);
+            auto p1 = poses[i], p2 = poses[i + 1];
+            glVertex3d(p1.translation()[0], p1.translation()[1], p1.translation()[2]);
+            glVertex3d(p2.translation()[0], p2.translation()[1], p2.translation()[2]);
+            glEnd();
+        }
+        pangolin::FinishFrame();
+        usleep(5000);   // sleep 5 ms
+    }
+}
+
+void cvR_eiR(const cv::Mat& cvR, Eigen::Matrix3d& eiR)
+{
+    eiR <<  cvR.at<double>(0, 0), cvR.at<double>(0, 1), cvR.at<double>(0, 2),
+            cvR.at<double>(1, 0), cvR.at<double>(1, 1), cvR.at<double>(1, 2),
+            cvR.at<double>(2, 0), cvR.at<double>(2, 1), cvR.at<double>(2, 2);
+}
+void cvt_eit(const cv::Mat& cvt, Eigen::Vector3d& eit)
+{
+    eit <<  cvt.at<double>(0, 0), cvt.at<double>(0, 1), cvt.at<double>(0, 2);
 }
